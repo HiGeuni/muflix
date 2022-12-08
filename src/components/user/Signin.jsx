5d76bebd42a971d6962f3153b7fd133750c7fa8f
Kim HyoGeun <59858440+HiGeuni@users.noreply.github.com>
2022-12-09 00:47:51 +0900

    Feat: Signup UI Develop

--- src/components/user/Signin.jsx
+++ src/components/user/Signin.jsx
@@ -1,11 +1,79 @@
 // react-hook-form을 이용해서 form을 만들기
 import { useForm } from "react-hook-form";
-import NewStyle from "styles/FormStyle";
+import styled from "styled-components";
 import Axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { api } from "config/api";
 import { IsLogin } from "App";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import image from "static/images.jpg";
+
+const Background = styled.div`
+    position: absolute;
+    display: block;
+    z-index: -1;
+    background-size: cover;
+    height: 100%;
+    width: 100%;
+    background-color: #000;
+    img{
+        height: 0%;
+        width: 0%;
+    }
+    @media only screen and (min-width: 740px){
+        img{
+            min-height: 100%;
+            min-width: 100%;
+        }
+    }
+`
+
+const NewStyle = styled.div`
+    display: block;
+    background-color: black;
+    margin: 5%;
+    @media only screen and (min-width: 740px){
+        background-color: rgba(0,0,0,.75);
+        padding: 60px;
+        max-width: 450px;
+        min-height: 100vh;
+        margin: auto;
+    }
+    h2{
+        color: white;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+    label{
+        margin : 5px;
+        font-size: 14px;
+        font-weight: 600;
+        color: white;
+    }
+    input{
+        margin: 4px;
+        border : 0;
+        border-radius: 2px;
+        padding: 10px;
+        color: #fff;
+        background: #333;
+        line-height: 18px;
+    }
+    .submitButton{
+        margin-top: 0.83em;
+        background-color: red;
+        color: white;
+        font-weight: 800;
+        font-size: 20px;
+    }
+    .isUser{
+        margin-top: 0.83em;;
+        color: gray;
+    }
+`
 
 const SignInForm = () => {  
     const {setIsLogin} = useContext(IsLogin);
@@ -30,6 +98,9 @@ const SignInForm = () => {
     const onInvalid = (data) => console.log(data, "onInvalid");
     return (
         <>
+            <Background>
+                <img src={image} alt="" />
+            </Background>
             <NewStyle>
                 <h2>로그인</h2>
                 <form onSubmit={handleSubmit(onValid, onInvalid)}>
@@ -46,8 +117,12 @@ const SignInForm = () => {
                         }})} 
                         placeholder="Password"
                     />
-                    <input type="submit" className="submitButton" value="등록" />
+                    <input type="submit" className="submitButton" value="로그인" />
                 </form>
+                <div className="isUser">
+                    회원이 아니신가요?  
+                    <Link>회원 가입 </Link>
+                </div>
             </NewStyle>
         </>
     )
