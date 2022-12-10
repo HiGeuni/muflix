3f537733827b052ebc706ef6c9970078a70072d7
Kim HyoGeun <59858440+HiGeuni@users.noreply.github.com>
2022-12-11 01:02:51 +0900

    Fix: Player Bug

--- src/components/playingMenu/AudioPlayer.jsx
+++ src/components/playingMenu/AudioPlayer.jsx
@@ -2,9 +2,98 @@ import React, { useState, useEffect, useRef } from 'react';
 import AudioControls from './AudioControls';
 import { musicState } from 'atoms/music';
 import { useRecoilState } from 'recoil';
-import './styles.css';
+// import './styles.css';
 import { api } from 'config/api';
-import Axios from 'axios';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  display: block;
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  input[type="range"] {
+    height: 5px;
+    -webkit-appearance: none;
+    width: 100%;
+    margin-bottom: 10px;
+    border-radius: 8px;
+    background: #3b7677;
+    transition: background 0.2s ease;
+    cursor: pointer;
+  }
+
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: red;
+  }
+
+  .audio-player {
+    max-width: 350px;
+    padding: 24px;
+    border-radius: 20px;
+    box-shadow: 0 28px 28px rgba(0, 0, 0, 0.25);
+    background-color: black;
+    margin: auto;
+    color: var(--white);
+  }
+
+  .artwork {
+    border-radius: 120px;
+    display: block;
+    margin: auto;
+    height: 200px;
+    width: 200px;
+  }
+
+  .track-info {
+    color: white;
+    text-align: center;
+    z-index: 1;
+    position: relative;
+  }
+
+  .title {
+    font-weight: 700;
+    margin-bottom: 4px;
+  }
+
+  .artist {
+    font-weight: 300;
+    margin-top: 0;
+  }
+
+  .audio-controls {
+    display: flex;
+    justify-content: space-between;
+    width: 75%;
+    margin: 0 auto 15px;
+  }
+
+  .audio-controls .prev svg,
+  .audio-controls .next svg {
+    width: 35px;
+    height: 35px;
+  }
+
+  .audio-controls .play svg,
+  .audio-controls .pause svg {
+    height: 40px;
+    width: 40px;
+  }
+
+  .audio-controls path {
+    fill: var(--white);
+  }
+`
 
 function AudioPlayer() {
   // isplaying, curPlaying
@@ -45,7 +134,6 @@ function AudioPlayer() {
     }));
     startTimer();
     setCurMusicData(curMusicState.playlist[index]);
-    console.log(curMusicData);
   };
 
   const startTimer = () => {
@@ -54,7 +142,16 @@ function AudioPlayer() {
 
     intervalRef.current = setInterval(() => {
       if (audioRef.current.ended) {
-        toNextTrack();
+        if(curMusicState.curPlaying === curMusicState.playlist.length - 1){
+            audioRef.current.pause();
+            setMusicState((prev) => ({
+                ...prev,
+                isPlaying: false
+            }));
+            setTrackProgress(0);
+        }else{
+            toNextTrack();
+        }
       } else {
         setTrackProgress(audioRef.current.currentTime);
       }
@@ -126,6 +223,7 @@ function AudioPlayer() {
 );
 
   return (
+    <StyledDiv>
     <div className="audio-player">
       <div className="track-info">
         <img
@@ -154,6 +252,7 @@ function AudioPlayer() {
         />
       </div>
     </div>
+    </StyledDiv>
   );
 }
 
